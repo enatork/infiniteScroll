9b16514e1d81dad46cea65ba5d75f7c94b097672
Van Le <kimvan.le@gmail.com>
2015-08-10 19:53:07 -0700

    rename

--- infinite-scroll.js
+++ infinite-scroll.js
@@ -1,30 +1,30 @@
-﻿//qt-infinite-scroll - {expression} - Expression to evaluate (usually a function call) when the bottom of the element approaches the bottom of the browser window.
-//qt-infinite-scroll-element - {class} - Class that denotes the DOM element to which to infinite scroll is triggered from.  
-//qt-infinite-scroll-distance (optional) - {number} - A number representing how close the bottom of the element must be to the bottom of the browser window before the expression specified by infinite-scroll is triggered.Measured in multiples of the qt-infinite-scroll-element height
-//qt-infinite-scroll-disabled (optional) - {boolean} - A boolean expression that, when true, indicates that the infininite scroll expression should not be evaluated even if all other conditions are met. 
-//qt-infinite-scroll-immediate-check (optional) - {boolean} - A boolean expression that indicates that the directive should check immediately to see if a scroll event would trigger an evaluation of the infinite scroll expression even if a scroll event does not occur. 
+﻿//infinite-scroll - {expression} - Expression to evaluate (usually a function call) when the bottom of the element approaches the bottom of the browser window.
+//infinite-scroll-element - {class} - Class that denotes the DOM element to which to infinite scroll is triggered from.  
+//infinite-scroll-distance (optional) - {number} - A number representing how close the bottom of the element must be to the bottom of the browser window before the expression specified by infinite-scroll is triggered.Measured in multiples of the infinite-scroll-element height
+//infinite-scroll-disabled (optional) - {boolean} - A boolean expression that, when true, indicates that the infininite scroll expression should not be evaluated even if all other conditions are met. 
+//infinite-scroll-immediate-check (optional) - {boolean} - A boolean expression that indicates that the directive should check immediately to see if a scroll event would trigger an evaluation of the infinite scroll expression even if a scroll event does not occur. 
 angular.module('common')
-    .directive('qtInfiniteScroll', [
+    .directive('infiniteScroll', [
   '$rootScope', '$window', '$timeout', function ($rootScope, $window, $timeout) {
       return {
           link: function (scope, elem, attrs) {
               var scrollElement;
-              if (attrs.qtInfiniteScrollElement) {
-                  scrollElement = angular.element('.' + attrs.qtInfiniteScrollElement);
+              if (attrs.infiniteScrollElement) {
+                  scrollElement = angular.element('.' + attrs.infiniteScrollElement);
               } else {
                   scrollElement = angular.element($window);
               }             
               var checkWhenEnabled, handler, scrollDistance, scrollEnabled;
               scrollDistance = 0;
-              if (attrs.qtInfiniteScrollDistance != null) {
-                  scope.$watch(attrs.qtInfiniteScrollDistance, function (value) {
+              if (attrs.infiniteScrollDistance != null) {
+                  scope.$watch(attrs.infiniteScrollDistance, function (value) {
                       return scrollDistance = parseInt(value, 10);
                   });
               }
               scrollEnabled = true;
               checkWhenEnabled = false;
-              if (attrs.qtInfiniteScrollDisabled != null) {
-                  scope.$watch(attrs.qtInfiniteScrollDisabled, function (value) {
+              if (attrs.infiniteScrollDisabled != null) {
+                  scope.$watch(attrs.infiniteScrollDisabled, function (value) {
                       scrollEnabled = !value;
                       if (scrollEnabled && checkWhenEnabled) {
                           checkWhenEnabled = false;
@@ -42,9 +42,9 @@ angular.module('common')
                   shouldScroll = remaining <= scrollElement.height() * scrollDistance;
                   if (shouldScroll && scrollEnabled) {
                       if ($rootScope.$$phase) {
-                          return scope.$eval(attrs.qtInfiniteScroll);
+                          return scope.$eval(attrs.infiniteScroll);
                       } else {
-                          return scope.$apply(attrs.qtInfiniteScroll);
+                          return scope.$apply(attrs.infiniteScroll);
                       }
                   } else if (shouldScroll) {
                       return checkWhenEnabled = true;
@@ -56,8 +56,8 @@ angular.module('common')
                   return scrollElement.off('scroll', handler);
               });
               return $timeout((function () {
-                  if (attrs.qtInfiniteScrollImmediateCheck) {
-                      if (scope.$eval(attrs.qtInfiniteScrollImmediateCheck)) {
+                  if (attrs.infiniteScrollImmediateCheck) {
+                      if (scope.$eval(attrs.infiniteScrollImmediateCheck)) {
                           return handler();
                       }
                   } else {
